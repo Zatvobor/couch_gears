b2c92c1956ae256f2017ac6bd66aec7841c47be8
Zatvor <a.zatvornitskiy@gmail.com>
2013-02-14 17:16:00 +0200

    Updated a database docs

--- lib/couch_gears/database.ex
+++ lib/couch_gears/database.ex
@@ -1,21 +1,25 @@
 defmodule CouchGears.Database do
   @moduledoc """
+  This module provides CRUD functions for managing either databases or documents.
+  The main important thing is a `database` module designed to be a 'instance' for certain DB (see examples below).
 
-  ## Code snippets:
-
-  1. from pure Erlang:
-
-      Db = 'Elixir-CouchGears-Database':open(<<"db">>).
-      Db:find(<<"x">>).
-
-      'Elixir-CouchGears-Database':find(<<"db">>, <<"x">>).
-
-  2. from pure Elixir:
+  ## Examples:
 
       db = CouchGears.Database.open("db")
       db.find("x")
+      db.close()
+
+  works similarly as a statement:
 
       CouchGears.Database.find("db", "x")
+
+  also you can use a `database` module from pure Erlang environment:
+
+      Db = 'Elixir-CouchGears-Database':open(<<"db">>),
+      Db:find(<<"x">>),
+      DB:close().
+
+      'Elixir-CouchGears-Database':find(<<"db">>, <<"x">>).
   """
 
   alias CouchGears.Database.Helpers, as: Helpers
@@ -27,15 +31,21 @@ defmodule CouchGears.Database do
   defrecordp :database, [:raw_db, :db]
 
 
-  @doc false
+  @doc """
+  Gets associated `db` record. Check a `include/couch_db.hrl` for details.
+  """
   def db(database(db: db)), do: db
 
-  @doc false
+  @doc """
+  Gets associated raw `db` record which has been returned from `couch_db:open_int/2`.
+  """
   def raw_db(database(raw_db: raw_db)), do: raw_db
 
-  @doc false
-  def open(name) do
-    db = do_open(name)
+  @doc """
+  Opens a `db` and returns a `database` instance or `:no_db_file` atom in case it doesn't exist.
+  """
+  def open(db_name) do
+    db = do_open(db_name)
 
     unless db == :no_db_file do
       db = database(raw_db: db, db: Db.new(db))
@@ -44,9 +54,11 @@ defmodule CouchGears.Database do
     db
   end
 
-  @doc false
-  def open!(name) do
-    db = open(name)
+  @doc """
+  Opens a `db` and returns a `database` instance or exception in case it doesn't exist.
+  """
+  def open!(db_name) do
+    db = open(db_name)
 
     if db == :no_db_file do
       raise "No db file"
@@ -55,37 +67,45 @@ defmodule CouchGears.Database do
     db
   end
 
-  @doc false
+  @doc """
+  Closes associated `db`.
+  """
   def close(database(raw_db: raw_db)) do
     :couch_db.close(raw_db)
     database()
   end
 
-  @doc false
-  def create_db(name) do
-    :couch_server.create(name, [])
+  @doc """
+  Creates a `db`.
+  """
+  def create_db(db_name, opts // []) do
+    :couch_server.create(db_name, opts)
   end
 
-  @doc false
-  def delete_db(name) do
-    :couch_server.delete(name, [])
+  @doc """
+  Deletes a `db`.
+  """
+  def delete_db(db_name, opts // []) do
+    :couch_server.delete(db_name, opts)
   end
 
-  @doc false
+  @doc """
+  Returns a `document` as a raw `list` or either `:no_db_file`/`:missing` atom.
+  """
   def find_raw(_db, :no_db_file), do: :no_db_file
 
-  @doc false
   def find_raw(doc_id, db) when is_record(db, CouchGears.Database) do
     { _, document } = do_find(doc_id, db)
     document
   end
 
-  @doc false
-  def find_raw(db, doc_id), do: find_raw(doc_id, open(db))
+  def find_raw(db_name, doc_id), do: find_raw(doc_id, open(db_name))
 
-  @doc false
-  def find(a, b) do
-    doc = find_raw(a, b)
+  @doc """
+  Returns a `document` as a `HashDict` or either `:no_db_file`/`:missing` atom.
+  """
+  def find(doc_id, db) do
+    doc = find_raw(doc_id, db)
 
     unless doc == :no_db_file || doc == :missing do
       doc = HashDict.new(doc, Helpers.from_list_to_hash_dict_transform)
@@ -94,66 +114,66 @@ defmodule CouchGears.Database do
     doc
   end
 
-  @doc false
+  @doc """
+  Returns a `document` as a `HashDict` or either `:no_db_file`/`:missing` atom.
+  """
+  def find_with_rev(doc_id, rev, :no_db_file), do: :no_db_file
+
   def find_with_rev(doc_id, rev, database(raw_db: raw_db)) do
     case :couch_db.open_doc_revs(raw_db, doc_id, make_rev(rev), []) do
       {:ok, [{:ok, doc}]} ->
         {body} = :couch_doc.to_json_obj(doc, [])
-
-        {:ok, body}
+        HashDict.new(body, Helpers.from_list_to_hash_dict_transform)
       _ ->
-        {:not_found, :missing}
+        :missing
     end
   end
 
-  @doc false
-  def find_with_rev(db, doc_id, rev) do
-    { _, document } = find_with_rev(doc_id, rev, open(db))
-    document
+  def find_with_rev(db, doc_id, rev), do: find_with_rev(doc_id, rev, open(db))
+
+  @doc """
+  Creates a `document`.
+  """
+  def create_doc(db_name, raw_doc) when is_list(raw_doc) do
+    create_doc(raw_doc, open(db_name))
   end
 
-  @doc false
+  def create_doc(db_name, hash_doc) when is_record(hash_doc, HashDict) do
+    create_doc(db_name, Helpers.from_hash_dict_to_list(hash_doc))
+  end
+
+  def create_doc(hash_doc, db) when is_record(hash_doc, HashDict) and is_record(db, CouchGears.Database) do
+    raw_doc = create_doc(Helpers.from_hash_dict_to_list(hash_doc), db)
+    HashDict.new(raw_doc, Helpers.from_list_to_hash_dict_transform)
+  end
+
+  # it'd be a much better (inside)
+  def create_doc(raw_doc, db) when is_list(raw_doc) and is_record(db, CouchGears.Database) do
+    update(raw_doc, db)
+    find_raw(raw_doc, db)
+  end
+
+  @doc """
+  Updates a particular `document` and return a `rev` string or `:conflict` atom.
+  """
   def update(raw_doc, database(raw_db: raw_db)) when is_list(raw_doc) do
     json_doc   = :couch_doc.from_json_obj({raw_doc})
     {:ok, rev} = :couch_db.update_doc(raw_db, json_doc, [])
     rev
   end
 
-  @doc false
   def update(hash_doc, db) when is_record(hash_doc, HashDict) and is_record(db, CouchGears.Database) do
     raw_doc = Helpers.from_hash_dict_to_list(hash_doc)
     update(raw_doc, db)
   end
 
-  @doc false
-  def update(db, raw_doc) when is_list(raw_doc), do: update(raw_doc, open(db))
+  def update(db_name, raw_doc) when is_list(raw_doc), do: update(raw_doc, open(db_name))
 
-  @doc false
-  def update(db, hash_doc) when is_record(hash_doc, HashDict), do: update(hash_doc, open(db))
-
-  @doc false
-  def create_doc(db, raw_doc) when is_list(raw_doc) do
-    create_doc(raw_doc, open(db))
-  end
-
-  @doc false
-  def create_doc(db, hash_doc) when is_record(hash_doc, HashDict) do
-    create_doc(db, Helpers.from_hash_dict_to_list(hash_doc))
-  end
-
-  @doc false
-  def create_doc(hash_doc, db) when is_record(hash_doc, HashDict) and is_record(db, CouchGears.Database) do
-    raw_doc = create_doc(Helpers.from_hash_dict_to_list(hash_doc), db)
-    HashDict.new(raw_doc, Helpers.from_list_to_hash_dict_transform)
-  end
-
-  @doc false
-  def create_doc(raw_doc, db) when is_list(raw_doc) and is_record(db, CouchGears.Database) do
-    update(raw_doc, db)
-    find_raw(raw_doc, db)
-  end
+  def update(db_name, hash_doc) when is_record(hash_doc, HashDict), do: update(hash_doc, open(db_name))
 
 
+
+  # Internal stuff
 
   defp do_open(name) do
     case :couch_db.open_int(to_binary(name), []) do
