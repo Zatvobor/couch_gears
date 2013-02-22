db7d35d62b6e49110a356ecadde899408ac5f8c0
Zatvor <a.zatvornitskiy@gmail.com>
2013-02-22 16:32:06 +0200

    Updated CouchGears.Logger implementation

--- lib/couch_gears/logger.ex
+++ lib/couch_gears/logger.ex
@@ -1,18 +1,31 @@
 defmodule CouchGears.Logger do
-  @moduledoc false
+  @moduledoc """
+  A module provides basic functions for logging. There are `debug`, `info`, `error` handlers
+  which uses a `couch_log` module. So, all `couch_gears` log messages integrated with CouchDB logging.
+  You could manage those log messages through generic CouchDB options.
+  """
 
   @doc false
   def debug(message, args // []) do
-    if :couch_log.debug_on(), do: :couch_log.debug(binary_to_list(message), args)
+    bump(:debug_on, :debug, message, args)
   end
 
   @doc false
   def info(message, args // []) do
-    if :couch_log.info_on(), do: :couch_log.info(binary_to_list(message), args)
+    bump(:info_on, :info, message, args)
   end
 
   @doc false
   def error(message, args // []) do
-    if :couch_log.error_on(), do: :couch_log.error(binary_to_list(message), args)
+    bump(:error_on, :error, message, args)
+  end
+
+
+  defp bump(check_fun, logger_fun, message, args) when is_function(check_fun) and is_function(logger_fun) do
+    if check_fun.(), do: logger_fun.(message, args)
+  end
+
+  defp bump(check_fun, logger_fun, message, args) do
+    bump(function(:couch_log, check_fun, 0), function(:couch_log, logger_fun, 2), binary_to_list(message), args)
   end
 end
