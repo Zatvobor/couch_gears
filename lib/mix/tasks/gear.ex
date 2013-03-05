ce8474c144e4e1fcebfeefe65b4c698f464ca580
Zatvor <a.zatvornitskiy@gmail.com>
2013-03-05 14:52:22 +0200

    Removed deprecated stuff and add more detais for handlers block

--- lib/mix/tasks/gear.ex
+++ lib/mix/tasks/gear.ex
@@ -110,7 +110,16 @@ defmodule Mix.Tasks.Gear do
     use CouchGears.App
 
     config :gear,
-      handlers: [ dbs: all ]
+      handlers: [
+        # Handles request which doesn't belong to any kind of `db` (`global` requests)
+        # global: true,
+
+        # Sets a particular `db` names which belongs to application
+        # dbs: [:a, :b]
+
+        # Sets a `dbs: :all` for handling requests for all available dbs
+        dbs: :all
+      ]
 
     config :dynamo,
       # The environment this Dynamo runs on
